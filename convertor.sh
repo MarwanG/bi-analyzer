3922f3f9aa54a71794c81b74da873ad8db0c97d8
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2015-03-04 12:05:24 +0100

    convertor

--- convertor.sh
+++ convertor.sh
@@ -5,15 +5,17 @@ echo "in directory $1"
 
 cd $1
 
-# for i in *.pcap
-# do 
-# 	tcpdump -tttt -n -q -r $i ip | awk '{print $4,$6}' | sed 's/://g' > $i.txt
-# done 
-#'{print $1$2,$4,$6,$7}'
-
-find . -name '*.pcap' -print0 | while IFS= read -r -d '' file
-do 
-    tcpdump -tttt -n -q -r $file ip | awk '{print $4,$6}' | sed 's/://g' > $file.txt
+for d in *;
+do
+	echo "entering $d" 
+	cd "$d" && ls
+	touch $d.txt
+	for i in *.pcap
+	do
+		tcpdump -tttt -n -q -r $i ip | awk '{print $1,$2,$4,$6,$7}'| sed 's/\(.*\):/\1/' >> $d.txt
+	done
+	echo "exiting $d"
+	cd ..
 done
 
 cd -
