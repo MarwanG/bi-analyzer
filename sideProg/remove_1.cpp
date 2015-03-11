cf2e972b66d19b46e746a1c1692e5405b589cd0a
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2015-03-11 16:52:18 +0100

    consider many files

--- sideProg/remove_1.cpp
+++ sideProg/remove_1.cpp
@@ -50,8 +50,7 @@ void read_write(set<string> list,string name,string new_name){
 }
 
 // GETTING LIST OF CHANNELS FROM FILE.
-map<string,set<string> > get_channels(string name){
-	map<string,set<string> > list;
+void get_channels(string name,map<string,set<string> > *list){
 	ifstream file(name.c_str());
 	string str;
 	string b;
@@ -84,54 +83,47 @@ map<string,set<string> > get_channels(string name){
     	n = std::count(b.begin(), b.end(), '.');
         size_t n_t = std::count(t.begin(), t.end(), '.');
     	if(n == 3 && n_t == 3){
-    		map<string,set<string> >::iterator it = list.find(b);
-    		if(it != list.end()){
-					list[b].insert(t);
+    		map<string,set<string> >::iterator it = list->find(b);
+    		if(it != list->end()){
+					list->operator[](b).insert(t);
 		    }else{
                     std::set<string> tmp;
                     tmp.insert(t);
-					list[b]=tmp;
+					list->operator[](b)=tmp;
 			}
-            it = list.find(t);
-            if(it != list.end()){
-                    list[t].insert(b);
+            it = list->find(t);
+            if(it != list->end()){
+                    list->operator[](t).insert(b);
             }else{
                     set<string> tmp;
                     tmp.insert(b);
-                    list.insert(make_pair(t, tmp));
+                    list->insert(make_pair(t, tmp));
             }
 		}
     }
-    return list;
 }
 
 int main(int argc,char* argv[]){
-	if(argc > 1){
-		cout << argv[1] << "\n";
-		map<string,set<string> > list = get_channels(argv[1]);
+	// if(argc > 1){
+		// cout << argv[1] << "\n";
+        string f1 = "";
+        string f2 = "";
+        string f3 = "";
+
+	   	map<string,set<string> > *list;
+        get_channels(f1,list); 
+        get_channels(f2,list);
+        get_channels(f3,list);
         set<string> list_tmp;
         map<string,set<string> >::iterator it;
-        for(it = list.begin() ; it != list.end() ; it++){
+        for(it = list->begin() ; it != list->end() ; it++){
             if(it->second.size() == 1){
+                cout << it->first << "\n";
                 list_tmp.insert(it->first);
             }else{
-                // list_tmp.insert(it->first);
-                // cout << it->first << "   " << it->second.size() << "\n";
+           
             }
         }
-        // cout << "ici \n";
-        // std::set<string>::iterator it1;
-        // for (it1 = list_tmp.begin(); it1 != list_tmp.end(); ++it1)
-        // {
-        //    cout << *it1 << "  " << list[*it1].size() << "\n  ";
-        //    set<string>::iterator it2;
-        //    set<string> tmp = list[*it1];
-        //    for (it2 = tmp.begin(); it2 != tmp.end(); ++it2)
-        //   {
-        //     cout << " == " << *it2;
-        //   }
-        //   cout << "\n";
-        // }
 		read_write(list_tmp,argv[1],argv[2]);
 	}
-}
+
