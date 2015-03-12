dbb8064b2fb0388809958e6e1435af4bd506d166
Marwan Ghanem <marwan.t.ghanem@gmail.com>
2015-03-12 23:15:42 +0100

    remove_1 suite

---
+++ sideProg/remove_1.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo "removing ips with degree 1"
+echo "in directory $1"
+echo "using file $2"
+
+
+cp $2 $1
+cd $1
+
+FILE=$2
+while read line; do
+	echo "removing $line"
+    sed -i '/$line/d' * 
+done < $FILE
+
+rm $2
+cd -
